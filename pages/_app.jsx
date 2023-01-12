1525eaf147aba2bb2b15268b1c93365b6c012222
Denys Lytvynenko <den.lytvynenko@temabit.com>
2023-01-12 15:26:28 +0200

    add material ui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,10 @@
 import MainLayout from "../src/components/Layout/MainLayout";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import "../styles/globals.css";
 import "../styles/general.sass";
 
